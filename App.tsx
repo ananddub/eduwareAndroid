bb871f800fe9e7e5c8a6aa1ae89aa8670c3db2b6
ananddub <duanand6@gmail.com>
2024-04-19 08:58:28 +0530

    fixed looking bad theme in Listing

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BasicDetail from "./Context/ProvideBasic";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
-import { Listing } from "./Components/Listing.1";
+import { Listing } from "./Components/Listing";
 const stack = createNativeStackNavigator();
-function App() {
+function App(): JSX.Element {
     return <Listing />;
     return (
         <BasicDetail>
--- Components/Listing.tsx
+++ Components/Listing.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, FlatList, Text } from "react-native";
+import { Dimensions, FlatList, Image, Text } from "react-native";
 import { View } from "react-native";
 import { TextInput } from "react-native";
 const data = Array(100)
@@ -6,8 +6,10 @@ const data = Array(100)
     .map((_, index) =>
         Object.create({
             id: index,
+            sec: "A",
             name: "Anand Kumar Dubey",
             class: "XI",
+            adm: "ADFSDGB1345",
             roll: "123",
             phone: "9876543210",
             status: "active",
@@ -15,6 +17,8 @@ const data = Array(100)
             text: `$index ${index}`,
         })
     );
+data[0].name = "anand";
+data[5].name = "temp";
 
 const renderItem = ({ item }: { item: any }) => {
     return (
@@ -23,34 +27,31 @@ const renderItem = ({ item }: { item: any }) => {
                 backgroundColor: "white",
                 padding: 10,
                 borderWidth: 1,
-                borderColor: "#E7E7E9",
-                // marginHorizontal: 5,
-                marginVertical: 1,
-                // borderRadius: 10,
+                borderColor: "#EAEBEF",
             }}
         >
             <View
                 style={{
                     flex: 1,
                     flexWrap: "wrap",
-
                     flexDirection: "row",
                 }}
             >
-                <View
+                <Image
+                    source={{ uri: "https://picsum.photos/200" }}
                     style={{
-                        backgroundColor: "#E7E7E9",
-                        height: 50,
                         width: 50,
+                        height: 50,
+                        borderRadius: 100 / 2,
                         marginRight: 10,
-                        borderRadius: 100,
                     }}
-                ></View>
+                />
+                <View></View>
                 <View
                     style={{
                         flex: 1,
                         flexDirection: "row",
-                        marginTop: 10,
+                        marginTop: 5,
                         justifyContent: "space-between",
                     }}
                 >
@@ -58,67 +59,65 @@ const renderItem = ({ item }: { item: any }) => {
                         <Text
                             style={{
                                 fontWeight: "bold",
-                                fontSize: 13,
+                                fontSize: 14,
                                 color: "#3C3C3C",
                             }}
                         >
                             {item.name}
                         </Text>
-                        <Text
-                            style={{
-                                color: "#414147",
-                                fontSize: 12,
-                                borderRadius: 10,
-                                borderWidth: 1,
-                                borderColor: "#D1D5DB",
-                                padding: 2,
-                                paddingHorizontal: 7,
-                            }}
-                        >
-                            {item.fname}
-                        </Text>
                     </View>
-                    <View>
-                        <Text
-                            style={{
-                                backgroundColor: "#D1FAE4",
-                                padding: 2,
-                                paddingHorizontal: 10,
-                                borderRadius: 10,
-                                borderColor: "#D3F2E1",
-                                color: "#227749",
-                            }}
-                        >
-                            {item.id}
-                        </Text>
-                    </View>
-                    <View>
-                        <Text
-                            style={{
-                                backgroundColor: "#FDEAD8",
-                                padding: 2,
-                                paddingHorizontal: 10,
-                                borderRadius: 10,
-                                borderColor: "#F5E2D0",
-                                color: "#AB531A",
-                            }}
-                        >
-                            {item.roll}
-                        </Text>
-                    </View>
-                    <View>
-                        <Text
-                            style={{
-                                backgroundColor: "#ECDFFB",
-                                padding: 2,
-                                paddingHorizontal: 10,
-                                borderRadius: 10,
-                                borderColor: "#E3D8F0",
-                                color: "#4C0F9F",
-                            }}
-                        >
-                            {item.class}
-                        </Text>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            width: 120,
+                        }}
+                    >
+                        <View>
+                            <Text
+                                style={{
+                                    backgroundColor: "#D1FAE4",
+                                    borderColor: "#D3F2E1",
+                                    color: "#227749",
+                                    padding: 2,
+                                    fontSize: 12,
+                                    paddingHorizontal: 10,
+                                    borderRadius: 10,
+                                }}
+                            >
+                                {item.sec}
+                            </Text>
+                        </View>
+                        <View>
+                            <Text
+                                style={{
+                                    backgroundColor: "#FDEAD8",
+                                    borderColor: "#F5E2D0",
+                                    color: "#AB531A",
+                                    padding: 2,
+                                    fontSize: 12,
+                                    paddingHorizontal: 10,
+                                    borderRadius: 10,
+                                }}
+                            >
+                                {item.roll}
+                            </Text>
+                        </View>
+                        <View>
+                            <Text
+                                style={{
+                                    backgroundColor: "#ECDFFB",
+                                    borderColor: "#E3D8F0",
+                                    color: "#4C0F9F",
+                                    fontSize: 12,
+                                    padding: 2,
+                                    paddingHorizontal: 10,
+                                    borderRadius: 10,
+                                }}
+                            >
+                                {item.class}
+                            </Text>
+                        </View>
                     </View>
                 </View>
                 {/* <View>
@@ -138,6 +137,41 @@ const renderItem = ({ item }: { item: any }) => {
                     </Text>
                 </View> */}
             </View>
+            <View
+                style={{
+                    flex: 1,
+                    position: "absolute",
+                    flexDirection: "row",
+                    marginTop: 45,
+                }}
+            >
+                <Text
+                    style={{
+                        marginLeft: 70,
+                        color: "#414147",
+                        fontSize: 12,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: "#D1D5DB",
+                        paddingHorizontal: 7,
+                    }}
+                >
+                    {item.fname}
+                </Text>
+                <Text
+                    style={{
+                        marginLeft: 10,
+                        color: "#414147",
+                        fontSize: 12,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: "#D1D5DB",
+                        paddingHorizontal: 7,
+                    }}
+                >
+                    {item.adm}
+                </Text>
+            </View>
         </View>
     );
 };
@@ -148,10 +182,95 @@ export const Listing = (): JSX.Element => {
 
     return (
         <View>
-            <FlatList
+            <View
                 style={{
                     marginTop: 100,
-                    backgroundColor: "#F6F6F6",
+                    flexDirection: "row",
+                    marginBottom: 5,
+                    paddingHorizontal: 6,
+                    marginHorizontal: 5,
+                    // justifyContent: "center",
+                }}
+            >
+                <TextInput
+                    style={{
+                        width: "100%",
+                        height: 40,
+                        backgroundColor: "#F5F5F5",
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: "#D1D5DB",
+                        paddingHorizontal: 10,
+                    }}
+                    placeholder="Search..."
+                />
+            </View>
+            {/* <View
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "space-between",
+                }}
+            >
+                <Text
+                    style={{
+                        marginLeft: 75,
+                    }}
+                >
+                    Name
+                </Text>
+                <View
+                    style={{
+                        width: 150,
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <Text
+                        style={{
+                            backgroundColor: "#D1FAE4",
+                            borderColor: "#D3F2E1",
+                            color: "#227749",
+                            borderRadius: 10,
+                            borderWidth: 1,
+                            paddingHorizontal: 5,
+                        }}
+                    >
+                        Sec
+                    </Text>
+                    <Text
+                        style={{
+                            borderRadius: 10,
+                            borderWidth: 1,
+
+                            backgroundColor: "#FDEAD8",
+                            borderColor: "#F5E2D0",
+                            color: "#AB531A",
+                            paddingHorizontal: 5,
+                        }}
+                    >
+                        Roll
+                    </Text>
+                    <Text
+                        style={{
+                            borderRadius: 10,
+                            borderWidth: 1,
+                            marginRight: 10,
+                            backgroundColor: "#ECDFFB",
+                            borderColor: "#E3D8F0",
+                            color: "#4C0F9F",
+                            paddingHorizontal: 5,
+                        }}
+                    >
+                        Class
+                    </Text>
+                </View>
+            </View> */}
+            <View style={{ padding: 2 }}></View>
+            <FlatList
+                style={{
+                    backgroundColor: "#F8F8FA",
                 }}
                 data={data}
                 renderItem={renderItem}
